31103428d5014565bdfdf11b407f4b4b3c23b8f1
Roman Oskolkov <wild_neo@mail.ru>
2019-09-22 22:18:23 +0500

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
+import Definitions from './Definitions/Definitions';
 import './App.css';
+// import logo from './logo.svg';
+
+const definitions = [
+  { dd: 'one', dt: 'two' },
+  { dd: 'another term', dt: 'another description' },
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="container" class="container m-3">
+      <Definitions data={definitions}/>
     </div>
   );
 }
